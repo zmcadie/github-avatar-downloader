1d653fb0c306c638749816ecff350ece3965137a
Zola <zolamcadie@gmail.com>
2017-07-03 23:58:06 +0000

    gets contributors to a repo using github username and token

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,13 @@ const request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+const GITHUB_USER = 'zmcadie';
+const GITHUB_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  request.get(`https://api.github.com/repos/${repoOwner}/${repoName}/contributors`)
+  const requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+  console.log(requestURL)
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
