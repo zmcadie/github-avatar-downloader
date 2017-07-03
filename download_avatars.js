0b94395cf21cf72c5f92bc6664ae4ea3181bf6f2
Zola <zolamcadie@gmail.com>
2017-07-03 23:48:26 +0000

    gets repository contributors for specified repo

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,12 @@
 const request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  request.get(`https://api.github.com/repos/${repoOwner}/${repoName}/contributors`)
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
