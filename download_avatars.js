0eef45821720074b3df392496e0954ffab39c0b2
Zola <zolamcadie@gmail.com>
2017-07-04 00:22:09 +0000

    prints the contributor JSON for given repo

--- download_avatars.js
+++ download_avatars.js
@@ -2,13 +2,20 @@ const request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-const GITHUB_USER = 'zmcadie';
+const GITHUB_USER = process.env.GITHUB_USER;
 const GITHUB_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
   const requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
-  console.log(requestURL)
+  const requestOptions = {
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader'
+    }
+  };
+  request.get(requestURL, requestOptions, (err, resp, body) => {
+    console.log(body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
